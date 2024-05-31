edab795846b3b96c8c53cc83c5e5c9e96227c06a
Kaowarstail <victor.dane92@gmail.com>
2024-05-31 16:01:14 +0200

    [TEST] Create game

--- symfony-blackjack/tests/Controller/GameControllerTest.php
+++ symfony-blackjack/tests/Controller/GameControllerTest.php
@@ -3,5 +3,23 @@ namespace App\Tests\Controller;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 class GameControllerTest extends WebTestCase{
-    
+    public function test__createGame(): void
+    {
+        $client = static::createClient();
+        $client->request('POST', '/login_check', [], [], ['CONTENT_TYPE' => 'application/json'], json_encode([
+            'username' => 'admin',
+            'password' => 'admin',
+        ]));
+
+        $response = $client->getResponse();
+        $data = json_decode($response->getContent(), true);
+        $jwt = $data['token'];
+
+        $client->request('POST', '/game', [], [], ['CONTENT_TYPE' => 'application/json', 'HTTP_Authorization' => 'Bearer '.$jwt]);
+        $this->assertEquals(200, $client->getResponse()->getStatusCode());
+
+        $client = static::createClient();
+        $client->request('POST', '/game', [], [], ['CONTENT_TYPE' => 'application/json']);
+        $this->assertEquals(401, $client->getResponse()->getStatusCode());
+    }
 }
