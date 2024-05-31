162003c52de8a34dd5323c43bdb3dced92e6be6d
charline <ch.royer15@gmail.com>
2024-05-31 15:30:17 +0200

    userControllerTEst

--- symfony-blackjack/tests/Controller/TurnControllerTest.php
+++ symfony-blackjack/tests/Controller/TurnControllerTest.php
@@ -4,4 +4,5 @@ use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
 
 class TurnControllerTest extends KernelTestCase{
     
+    
 }
--- symfony-blackjack/tests/Controller/UserControllerTest.php
+++ symfony-blackjack/tests/Controller/UserControllerTest.php
@@ -1,8 +1,34 @@
 <?php
 namespace App\Tests\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
-class UserControllerTest extends KernelTestCase{
+class UserControllerTest extends WebTestCase{
+
+
+    public function test__createUser(): void
+    {
+
+        $client = static::createClient();
+        $body = json_encode([
+            'email' => 'test@test.fr',
+            'password' => 'test111',
+            'username' => 'test111'
+        ]);
+
+        $client->request('POST', '/user', [], [], ['CONTENT_TYPE' => 'application/json'], $body);
+     
+        $this->assertEquals(201, $client->getResponse()->getStatusCode());
+    }
     
+    private static function dataProvider_createUser(): array
+    {
+        return [
+            ['POST', 201],
+            ['GET' ]
+            ['DELETE']
+            ['PATCH'],
+            ['PUT']
+        ];
+    }
 }
